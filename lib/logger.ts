2062d48042298acc38ddd16baa5f054a6d1a3051
elmoustafi-22 <mustophaqadir@gmail.com>
2025-12-12 19:33:51 +0100

    update logger implementation to use Winston; upgrade Next.js version and add Winston as a dependency

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,15 +1,17 @@
-import pino from 'pino';
+import winston from "winston";
 
-const logger = pino({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname',
-    },
-  },
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] ${level}: ${message}`;
+    })
+  ),
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
